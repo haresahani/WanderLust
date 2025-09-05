63b9dde631416fd8b501dec946cebdfd87142e4e
Hare Sahani <harecareer@gmail.com>
2025-09-05 17:55:37 +0530

    Create Reviews

--- models/listing.js
+++ models/listing.js
@@ -32,6 +32,12 @@ const listingSchema = new Schema({
     price: Number,
     location: String,
     country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
 
 });
 
