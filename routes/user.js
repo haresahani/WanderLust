282678f704dfee0b64d9b8eacbd7c0442daf9551
Hare Sahani <harecareer@gmail.com>
2025-09-13 18:21:38 +0530

    redirectUrl

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
@@ -14,8 +15,15 @@ router.post("/signup", wrapAsync(async (req, res) => {
         const newUser = new User({ username, email });
         const registeredUser = await User.register(newUser, password);
         console.log(registeredUser);
-        req.flash("success", "Welcome to WanderLust! Your account has been created successfully.");
-        res.redirect("/listings");
+
+        // Log the user in after successful registration
+        req.login(registeredUser, (err) => {
+            if(err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to WanderLust! Your account has been created successfully.");
+            res.redirect("/listings");
+        })
     } catch (err) {
         req.flash("error", err.message);
         res.redirect("/signup");
@@ -27,13 +35,15 @@ router.get("/login", (req, res) => {
 })
 
 router.post("/login",
+    saveRedirectUrl,
     passport.authenticate("local", {  // checks the username/password against MongoDB.
         failureRedirect: "/login",
         failureFlash: true // makes sure invalid credentials send the user back to /login with a flash error.
     }),
     (req, res) => {
         req.flash("success", `Welcome back, ${req.user.username}!`);
-        res.redirect("/listings");
+        let redirectUrl = res.locals.redirect || "/listings";
+        res.redirect(redirectUrl);
     }
 );
 
