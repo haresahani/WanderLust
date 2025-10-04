5bd34c74bd4014d182329cf5226213b93306a8c3
Hare Sahani <harecareer@gmail.com>
2025-10-04 18:58:30 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.get("/", (req, res) => res.render("home.ejs"));
+// app.get("/", (req, res) => res.render("home.ejs"));
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
