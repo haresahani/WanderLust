87671478bee568f72a29cc7faa14d3a72086c60e
Hare Sahani <harecareer@gmail.com>
2025-10-04 19:03:14 +0530

    render

--- app.js
+++ app.js
@@ -1,120 +1,120 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
 
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const path = require("path");
-const methodOverride = require("method-override");
-const engine = require("ejs-mate");
-const session = require("express-session");
-const MongoStore = require("connect-mongo");
-const flash = require("connect-flash");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
+// const express = require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+// const path = require("path");
+// const methodOverride = require("method-override");
+// const engine = require("ejs-mate");
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo");
+// const flash = require("connect-flash");
+// const passport = require("passport");
+// const LocalStrategy = require("passport-local");
 
-// Middleware
-const { isLoggedIn } = require("./middleware.js");
+// // Middleware
+// const { isLoggedIn } = require("./middleware.js");
 
-// Models
-const Listing = require("./models/listing.js");
-const Review = require("./models/review.js");
-const User = require("./models/user.js");
+// // Models
+// const Listing = require("./models/listing.js");
+// const Review = require("./models/review.js");
+// const User = require("./models/user.js");
 
-// Routers
-const listingRouter = require("./routes/listing.js");
-const reviewRouter = require("./routes/review.js");
-const userRouter = require("./routes/user.js");
+// // Routers
+// const listingRouter = require("./routes/listing.js");
+// const reviewRouter = require("./routes/review.js");
+// const userRouter = require("./routes/user.js");
 
-// Utils
-const wrapAsync = require("./utils/wrapAsync.js");
-const ExpressError = require("./utils/ExpressError.js");
+// // Utils
+// const wrapAsync = require("./utils/wrapAsync.js");
+// const ExpressError = require("./utils/ExpressError.js");
 
-// MongoDB connection URL
-const dbURL = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
+// // MongoDB connection URL
+// const dbURL = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
 
-// Connect to MongoDB
-mongoose.connect(dbURL)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.log("MongoDB connection error:", err));
+// // Connect to MongoDB
+// mongoose.connect(dbURL)
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch(err => console.log("MongoDB connection error:", err));
 
-// App Config
-app.engine("ejs", engine);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+// // App Config
+// app.engine("ejs", engine);
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(methodOverride("_method"));
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(methodOverride("_method"));
 
-// Session store
-const store = MongoStore.create({
-  mongoUrl: dbURL,
-  crypto: {
-    secret: process.env.SECRET, 
-  },
-  touchAfter: 24 * 3600, // Update session only once per 24 hours
-});
+// // Session store
+// const store = MongoStore.create({
+//   mongoUrl: dbURL,
+//   crypto: {
+//     secret: process.env.SECRET, 
+//   },
+//   touchAfter: 24 * 3600, // Update session only once per 24 hours
+// });
 
-store.on("error", (e) => {
-  console.log("SESSION STORE ERROR", e);
-});
+// store.on("error", (e) => {
+//   console.log("SESSION STORE ERROR", e);
+// });
 
-// Session configuration
-const sessionOption = {
-  store,
-  name: "session",
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-  },
-};
+// // Session configuration
+// const sessionOption = {
+//   store,
+//   name: "session",
+//   secret: process.env.SECRET,
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: {
+//     httpOnly: true,
+//     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+//     maxAge: 7 * 24 * 60 * 60 * 1000,
+//   },
+// };
 
-app.use(session(sessionOption));
-app.use(flash());
+// app.use(session(sessionOption));
+// app.use(flash());
 
-// Passport configuration
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// // Passport configuration
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
-// Flash and current user middleware
-app.use((req, res, next) => {
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  res.locals.currUser = req.user || null;
-  next();
-});
+// // Flash and current user middleware
+// app.use((req, res, next) => {
+//   res.locals.success = req.flash("success");
+//   res.locals.error = req.flash("error");
+//   res.locals.currUser = req.user || null;
+//   next();
+// });
 
-// Routes 
-// app.get("/", (req, res) => res.render("home.ejs"));
+// // Routes 
+// // app.get("/", (req, res) => res.render("home.ejs"));
 
-app.use("/listings", listingRouter);
-app.use("/listings/:id/reviews", reviewRouter);
-app.use("/", userRouter);
+// app.use("/listings", listingRouter);
+// app.use("/listings/:id/reviews", reviewRouter);
+// app.use("/", userRouter);
 
-// 404 handler
-app.use((req, res, next) => {
-  next(new ExpressError(404, "Page Not Found!"));
-});
+// // 404 handler
+// app.use((req, res, next) => {
+//   next(new ExpressError(404, "Page Not Found!"));
+// });
 
-// Global error handler
-app.use((err, req, res, next) => {
-  console.error("Error details:", err);
-  const { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).render("error.ejs", { statusCode, message });
-});
+// // Global error handler
+// app.use((err, req, res, next) => {
+//   console.error("Error details:", err);
+//   const { statusCode = 500, message = "Something went wrong" } = err;
+//   res.status(statusCode).render("error.ejs", { statusCode, message });
+// });
 
-// Start server
-const PORT = process.env.PORT || 9000;
-app.listen(PORT, () => {
-  console.log(`App is running on port ${PORT}`);
-});
+// // Start server
+// const PORT = process.env.PORT || 9000;
+// app.listen(PORT, () => {
+//   console.log(`App is running on port ${PORT}`);
+// });
