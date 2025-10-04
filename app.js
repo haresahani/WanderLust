9725a7b1b458377aab76c6144f669ee72098e5f4
Hare Sahani <harecareer@gmail.com>
2025-10-04 19:07:41 +0530

    render error fix

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.use((req, res, next) => {
 });
 
 // Routes 
-// app.get("/", (req, res) => res.render("home.ejs"));
+app.get("/", (req, res) => res.redirect("/listings"));
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
